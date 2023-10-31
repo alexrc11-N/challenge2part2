886b92c1fdff5943544f56b961781e34f723a38d
alexrc11-N <143229102+alexrc11-N@users.noreply.github.com>
2023-10-31 18:16:39 -0500

    Create main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+/****************
+Name: Alex Rosado
+Challenge 2 par 2
+parameters and libraries needed to run main.cpp
+*******/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "hash.h"
+#include <iostream>
+
+int main();
+
+#endif // MAIN_H
